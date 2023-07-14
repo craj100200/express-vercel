5e37471c8660ed35d2964e77adb5684188c42729
craj100200 <craj100200@gmail.com>
2023-07-14 19:19:40 +0000

    first commit

---
+++ api/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const port  = 3000;
+
+const app = express();
+
+
+app.get('hello-world', (request, response) =>  { 
+	response.send("Hello, World!");
+});
+
+app.listen(port , () => {console.log("server running!");});
+
+module.exports = app;
